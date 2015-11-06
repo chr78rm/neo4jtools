520727328dfdd50ab1ec15423354595ac71f9f16
chr78rm <chr78rm@onlinehome.de>
2015-11-06 15:24:01 +0100

    checkConstraint method implemented.

--- neo4jtools-objectgraphmapper/src/main/java/de/christofreichardt/neo4jtools/ogm/SingleLinkData.java
+++ neo4jtools-objectgraphmapper/src/main/java/de/christofreichardt/neo4jtools/ogm/SingleLinkData.java
@@ -4,7 +4,13 @@
  */
 package de.christofreichardt.neo4jtools.ogm;
 
+import de.christofreichardt.diagnosis.AbstractTracer;
+import java.util.Set;
 import org.neo4j.graphdb.Direction;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.RelationshipType;
 
 /**
  *
@@ -21,6 +27,39 @@ public class SingleLinkData extends LinkData {
 
   public boolean isNullable() {
     return nullable;
+  }
+  
+  <T extends Enum<T> & RelationshipType> boolean checkConstraint(Node entityNode, Class<T> relationshipTypes, MappingInfo mappingInfo) throws ClassNotFoundException {
+    AbstractTracer tracer = getCurrentTracer();
+    tracer.entry("boolean", this, "checkConstraint(Node entityNode, Class<T> relationshipTypes, MappingInfo mappingInfo)");
+    
+    try {
+      tracer.out().printfIndentln("this = %s", this);
+      
+      Set<String> labels = mappingInfo.getLabels(this.linkedEntityClass);
+      RelationshipType relationshipType = Enum.valueOf(relationshipTypes, this.type);
+      Iterable<Relationship> relationships = entityNode.getRelationships(relationshipType, this.direction);
+      int counter = 0;
+      for (Relationship relationship : relationships) {
+        Node otherNode = relationship.getOtherNode(entityNode);
+        boolean labelFlag = false;
+        for (Label label : otherNode.getLabels()) {
+          if (labels.contains(label.name())) {
+            labelFlag = true;
+            break;
+          }
+        }
+        if (labelFlag)
+          counter++;
+      }
+      
+      tracer.out().printfIndentln("counter = %d", counter);
+      
+      return counter <= 1;
+    }
+    finally {
+      tracer.wayout();
+    }
   }
 
   @Override
