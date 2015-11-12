82f8b7063f2b832961a12c4801183eb69cfc2702
chr78rm <chr78rm@onlinehome.de>
2015-11-12 15:21:26 +0100

    Common code refactored into BasicMapperUnit.

--- neo4jtools-objectgraphmapper/src/test/java/de/christofreichardt/neo4jtools/ogm/Node2ObjectMapperUnit.java
+++ neo4jtools-objectgraphmapper/src/test/java/de/christofreichardt/neo4jtools/ogm/Node2ObjectMapperUnit.java
@@ -7,9 +7,6 @@
 package de.christofreichardt.neo4jtools.ogm;
 
 import de.christofreichardt.diagnosis.AbstractTracer;
-import de.christofreichardt.diagnosis.Traceable;
-import de.christofreichardt.diagnosis.TracerFactory;
-import de.christofreichardt.neo4jtools.idgen.IdGenLabels;
 import de.christofreichardt.neo4jtools.ogm.model.Account;
 import de.christofreichardt.neo4jtools.ogm.model.Account2;
 import de.christofreichardt.neo4jtools.ogm.model.Document;
@@ -29,106 +26,94 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Properties;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Path;
-import org.neo4j.graphdb.ResourceIterable;
 import org.neo4j.graphdb.ResourceIterator;
 import org.neo4j.graphdb.Transaction;
-import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-import org.neo4j.graphdb.factory.GraphDatabaseSettings;
-import org.neo4j.graphdb.schema.IndexDefinition;
-import org.neo4j.graphdb.schema.Schema;
 import org.neo4j.graphdb.traversal.Evaluation;
-import org.neo4j.graphdb.traversal.Evaluator;
 import org.neo4j.graphdb.traversal.Traverser;
 import org.neo4j.graphdb.traversal.Uniqueness;
-import org.neo4j.tooling.GlobalGraphOperations;
 
 /**
  *
  * @author Christof Reichardt
  */
-public class Node2ObjectMapperUnit implements Traceable {
-  static private GraphDatabaseService graphDatabaseService;
-  final static String DB_PATH = "." + File.separator + "db";
-  final private Properties properties;
+public class Node2ObjectMapperUnit extends BasicMapperUnit {
+//  static private GraphDatabaseService graphDatabaseService;
+//  final static String DB_PATH = "." + File.separator + "db";
+//  final private Properties properties;
 
   public Node2ObjectMapperUnit(Properties properties) {
-    this.properties = properties;
+    super(properties);
   }
   
-  @BeforeClass
-  static public void startDB() {
-    AbstractTracer tracer = TracerFactory.getInstance().getCurrentPoolTracer();
-    tracer.entry("void", Node2ObjectMapperUnit.class, "startDB()");
-    
-    try {
-      Node2ObjectMapperUnit.graphDatabaseService = new GraphDatabaseFactory()
-          .newEmbeddedDatabaseBuilder(DB_PATH)
-          .setConfig(GraphDatabaseSettings.keep_logical_logs, "10 files")
-          .newGraphDatabase();
-      try (Transaction transaction = Node2ObjectMapperUnit.graphDatabaseService.beginTx()) {
-        Schema schema = Node2ObjectMapperUnit.graphDatabaseService.schema();
-        for (IndexDefinition index : schema.getIndexes()) {
-          index.drop();
-        }
-        schema.indexFor(RESTfulCryptoLabels.ACCOUNTS)
-            .on("commonName")
-            .create();
-        schema.indexFor(RESTfulCryptoLabels.DOCUMENTS)
-            .on("id")
-            .create();
-        schema.indexFor(RESTfulCryptoLabels.KEY_RINGS)
-            .on("id")
-            .create();
-        schema.indexFor(RESTfulCryptoLabels.KEY_ITEMS)
-            .on("id")
-            .create();
-        schema.indexFor(RESTfulCryptoLabels.ROLES)
-            .on("id")
-            .create();
-        transaction.success();
-      }
-    }
-    finally {
-      tracer.wayout();
-    }
-  }
-  
-  @Before
-  public void init() {
-    AbstractTracer tracer = getCurrentTracer();
-    tracer.entry("void", this, "init()");
-
-    try {
-      try (Transaction transaction = Node2ObjectMapperUnit.graphDatabaseService.beginTx()) {
-        ResourceIterable<Node> nodes = GlobalGraphOperations.at(Node2ObjectMapperUnit.graphDatabaseService).getAllNodes();
-        nodes.forEach(node -> {
-          tracer.out().printfIndentln("Delete: node[%d].", node.getId());
-          tracer.out().printfIndentln("node.getDegree() = %d", node.getDegree());
-          node.getRelationships().forEach(relationShip -> {
-            tracer.out().printfIndentln("Delete: relationShip[%d].", relationShip.getId());
-            relationShip.delete();
-          });
-          node.delete();
-        });
-        
-        Assert.assertTrue("Expected an empty database.", !GlobalGraphOperations.at(Node2ObjectMapperUnit.graphDatabaseService).getAllNodes().iterator().hasNext());
-        
-        transaction.success();
-      }
-    }
-    finally {
-      tracer.wayout();
-    }
-  }
+//  @BeforeClass
+//  static public void startDB() {
+//    AbstractTracer tracer = TracerFactory.getInstance().getCurrentPoolTracer();
+//    tracer.entry("void", Node2ObjectMapperUnit.class, "startDB()");
+//    
+//    try {
+//      Node2ObjectMapperUnit.graphDatabaseService = new GraphDatabaseFactory()
+//          .newEmbeddedDatabaseBuilder(DB_PATH)
+//          .setConfig(GraphDatabaseSettings.keep_logical_logs, "10 files")
+//          .newGraphDatabase();
+//      try (Transaction transaction = Node2ObjectMapperUnit.graphDatabaseService.beginTx()) {
+//        Schema schema = Node2ObjectMapperUnit.graphDatabaseService.schema();
+//        for (IndexDefinition index : schema.getIndexes()) {
+//          index.drop();
+//        }
+//        schema.indexFor(RESTfulCryptoLabels.ACCOUNTS)
+//            .on("commonName")
+//            .create();
+//        schema.indexFor(RESTfulCryptoLabels.DOCUMENTS)
+//            .on("id")
+//            .create();
+//        schema.indexFor(RESTfulCryptoLabels.KEY_RINGS)
+//            .on("id")
+//            .create();
+//        schema.indexFor(RESTfulCryptoLabels.KEY_ITEMS)
+//            .on("id")
+//            .create();
+//        schema.indexFor(RESTfulCryptoLabels.ROLES)
+//            .on("id")
+//            .create();
+//        transaction.success();
+//      }
+//    }
+//    finally {
+//      tracer.wayout();
+//    }
+//  }
+//  
+//  @Before
+//  public void init() {
+//    AbstractTracer tracer = getCurrentTracer();
+//    tracer.entry("void", this, "init()");
+//
+//    try {
+//      try (Transaction transaction = Node2ObjectMapperUnit.graphDatabaseService.beginTx()) {
+//        ResourceIterable<Node> nodes = GlobalGraphOperations.at(Node2ObjectMapperUnit.graphDatabaseService).getAllNodes();
+//        nodes.forEach(node -> {
+//          tracer.out().printfIndentln("Delete: node[%d].", node.getId());
+//          tracer.out().printfIndentln("node.getDegree() = %d", node.getDegree());
+//          node.getRelationships().forEach(relationShip -> {
+//            tracer.out().printfIndentln("Delete: relationShip[%d].", relationShip.getId());
+//            relationShip.delete();
+//          });
+//          node.delete();
+//        });
+//        
+//        Assert.assertTrue("Expected an empty database.", !GlobalGraphOperations.at(Node2ObjectMapperUnit.graphDatabaseService).getAllNodes().iterator().hasNext());
+//        
+//        transaction.success();
+//      }
+//    }
+//    finally {
+//      tracer.wayout();
+//    }
+//  }
   
   @Test
   public void indexDefinitions() {
@@ -504,59 +489,59 @@ public class Node2ObjectMapperUnit implements Traceable {
     }
   }
   
-  private void traceAllNodes() {
-    AbstractTracer tracer = getCurrentTracer();
-    tracer.entry("void", this, "traceAllNodes()");
-    
-    try {
-      try (Transaction transaction = Node2ObjectMapperUnit.graphDatabaseService.beginTx()) {
-        for (RESTfulCryptoLabels label : RESTfulCryptoLabels.values()) {
-          tracer.out().printfIndentln("--> label: %s", label);
-          Node2ObjectMapperUnit.graphDatabaseService.findNodes(label).forEachRemaining(node -> {
-            RichNode richNode = new RichNode(node);
-            richNode.trace(tracer);
-          });
-        }
-        Node2ObjectMapperUnit.graphDatabaseService.findNodes(IdGenLabels.ID_GENERATOR).forEachRemaining(node -> {
-          RichNode richNode = new RichNode(node);
-          richNode.trace(tracer);
-        });
-        transaction.success();
-      }
-    }
-    finally {
-      tracer.wayout();
-    }
-  }
-  
-  @After
-  public void exit() {
-    AbstractTracer tracer = getCurrentTracer();
-    tracer.entry("void", this, "exit()");
-    
-    try {
-    }
-    finally {
-      tracer.wayout();
-    }
-  }
-  
-  @AfterClass
-  static public void shutdownDB() {
-    AbstractTracer tracer = TracerFactory.getInstance().getCurrentPoolTracer();
-    tracer.entry("void", Node2ObjectMapperUnit.class, "shutdownDB()");
-    
-    try {
-      Node2ObjectMapperUnit.graphDatabaseService.shutdown();
-    }
-    finally {
-      tracer.wayout();
-    }
-  }
-
-  @Override
-  public AbstractTracer getCurrentTracer() {
-    return TracerFactory.getInstance().getCurrentPoolTracer();
-  }
+//  private void traceAllNodes() {
+//    AbstractTracer tracer = getCurrentTracer();
+//    tracer.entry("void", this, "traceAllNodes()");
+//    
+//    try {
+//      try (Transaction transaction = Node2ObjectMapperUnit.graphDatabaseService.beginTx()) {
+//        for (RESTfulCryptoLabels label : RESTfulCryptoLabels.values()) {
+//          tracer.out().printfIndentln("--> label: %s", label);
+//          Node2ObjectMapperUnit.graphDatabaseService.findNodes(label).forEachRemaining(node -> {
+//            RichNode richNode = new RichNode(node);
+//            richNode.trace(tracer);
+//          });
+//        }
+//        Node2ObjectMapperUnit.graphDatabaseService.findNodes(IdGenLabels.ID_GENERATOR).forEachRemaining(node -> {
+//          RichNode richNode = new RichNode(node);
+//          richNode.trace(tracer);
+//        });
+//        transaction.success();
+//      }
+//    }
+//    finally {
+//      tracer.wayout();
+//    }
+//  }
+//  
+//  @After
+//  public void exit() {
+//    AbstractTracer tracer = getCurrentTracer();
+//    tracer.entry("void", this, "exit()");
+//    
+//    try {
+//    }
+//    finally {
+//      tracer.wayout();
+//    }
+//  }
+//  
+//  @AfterClass
+//  static public void shutdownDB() {
+//    AbstractTracer tracer = TracerFactory.getInstance().getCurrentPoolTracer();
+//    tracer.entry("void", Node2ObjectMapperUnit.class, "shutdownDB()");
+//    
+//    try {
+//      Node2ObjectMapperUnit.graphDatabaseService.shutdown();
+//    }
+//    finally {
+//      tracer.wayout();
+//    }
+//  }
+//
+//  @Override
+//  public AbstractTracer getCurrentTracer() {
+//    return TracerFactory.getInstance().getCurrentPoolTracer();
+//  }
 
 }
--- neo4jtools-objectgraphmapper/src/test/java/de/christofreichardt/neo4jtools/ogm/Object2NodeMapperUnit.java
+++ neo4jtools-objectgraphmapper/src/test/java/de/christofreichardt/neo4jtools/ogm/Object2NodeMapperUnit.java
@@ -1,9 +1,6 @@
 package de.christofreichardt.neo4jtools.ogm;
 
 import de.christofreichardt.diagnosis.AbstractTracer;
-import de.christofreichardt.diagnosis.Traceable;
-import de.christofreichardt.diagnosis.TracerFactory;
-import de.christofreichardt.neo4jtools.idgen.IdGenLabels;
 import de.christofreichardt.neo4jtools.idgen.IdGeneratorService;
 import de.christofreichardt.neo4jtools.ogm.model.Account;
 import de.christofreichardt.neo4jtools.ogm.model.Account1;
@@ -24,93 +21,25 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.neo4j.graphdb.Direction;
-import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.ResourceIterable;
 import org.neo4j.graphdb.Transaction;
-import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-import org.neo4j.graphdb.factory.GraphDatabaseSettings;
-import org.neo4j.graphdb.schema.IndexDefinition;
-import org.neo4j.graphdb.schema.Schema;
-import org.neo4j.tooling.GlobalGraphOperations;
 
 /**
  *
  * @author Christof Reichardt
  */
-public class Object2NodeMapperUnit implements Traceable {
-  static private GraphDatabaseService graphDatabaseService;
-  final static String DB_PATH = "." + File.separator + "db";
-  final private Properties properties;
+public class Object2NodeMapperUnit extends BasicMapperUnit {
   
   @Rule
   public ExpectedException thrown = ExpectedException.none();
   
   public Object2NodeMapperUnit(Properties properties) {
-    this.properties = properties;
-  }
-  
-  @BeforeClass
-  static public void startDB() {
-    AbstractTracer tracer = TracerFactory.getInstance().getCurrentPoolTracer();
-    tracer.entry("void", Object2NodeMapperUnit.class, "startDB()");
-    
-    try {
-      Object2NodeMapperUnit.graphDatabaseService = new GraphDatabaseFactory()
-          .newEmbeddedDatabaseBuilder(DB_PATH)
-          .setConfig(GraphDatabaseSettings.keep_logical_logs, "10 files")
-          .newGraphDatabase();
-      try (Transaction transaction = Object2NodeMapperUnit.graphDatabaseService.beginTx()) {
-        Schema schema = Object2NodeMapperUnit.graphDatabaseService.schema();
-        for (IndexDefinition index : schema.getIndexes()) {
-          index.drop();
-        }
-        schema.indexFor(RESTfulCryptoLabels.ACCOUNTS)
-            .on("commonName")
-            .create();
-        transaction.success();
-      }
-    }
-    finally {
-      tracer.wayout();
-    }
-  }
-  
-  @Before
-  public void init() {
-    AbstractTracer tracer = getCurrentTracer();
-    tracer.entry("void", this, "init()");
-
-    try {
-      try (Transaction transaction = Object2NodeMapperUnit.graphDatabaseService.beginTx()) {
-        ResourceIterable<Node> nodes = GlobalGraphOperations.at(Object2NodeMapperUnit.graphDatabaseService).getAllNodes();
-        nodes.forEach(node -> {
-          tracer.out().printfIndentln("Delete: node[%d].", node.getId());
-          tracer.out().printfIndentln("node.getDegree() = %d", node.getDegree());
-          node.getRelationships().forEach(relationShip -> {
-            tracer.out().printfIndentln("Delete: relationShip[%d].", relationShip.getId());
-            relationShip.delete();
-          });
-          node.delete();
-        });
-        
-        Assert.assertTrue("Expected an empty database.", !GlobalGraphOperations.at(Object2NodeMapperUnit.graphDatabaseService).getAllNodes().iterator().hasNext());
-        
-        transaction.success();
-      }
-    }
-    finally {
-      tracer.wayout();
-    }
+    super(properties);
   }
   
   @Test
@@ -580,31 +509,6 @@ public class Object2NodeMapperUnit implements Traceable {
     }
   }
   
-  private void traceAllNodes() {
-    AbstractTracer tracer = getCurrentTracer();
-    tracer.entry("void", this, "traceAllNodes()");
-    
-    try {
-      try (Transaction transaction = Object2NodeMapperUnit.graphDatabaseService.beginTx()) {
-        for (RESTfulCryptoLabels label : RESTfulCryptoLabels.values()) {
-          tracer.out().printfIndentln("--> label: %s", label);
-          Object2NodeMapperUnit.graphDatabaseService.findNodes(label).forEachRemaining(node -> {
-            RichNode richNode = new RichNode(node);
-            richNode.trace(tracer);
-          });
-        }
-        Object2NodeMapperUnit.graphDatabaseService.findNodes(IdGenLabels.ID_GENERATOR).forEachRemaining(node -> {
-          RichNode richNode = new RichNode(node);
-          richNode.trace(tracer);
-        });
-        transaction.success();
-      }
-    }
-    finally {
-      tracer.wayout();
-    }
-  }
-  
   private void checkNodes(int scenarioNr) {
     AbstractTracer tracer = getCurrentTracer();
     tracer.entry("void", this, "checkNodes(int scenarioNr)");
@@ -731,33 +635,4 @@ public class Object2NodeMapperUnit implements Traceable {
     }
   }
   
-  @After
-  public void exit() {
-    AbstractTracer tracer = getCurrentTracer();
-    tracer.entry("void", this, "exit()");
-    
-    try {
-    }
-    finally {
-      tracer.wayout();
-    }
-  }
-  
-  @AfterClass
-  static public void shutdownDB() {
-    AbstractTracer tracer = TracerFactory.getInstance().getCurrentPoolTracer();
-    tracer.entry("void", Object2NodeMapperUnit.class, "shutdownDB()");
-    
-    try {
-      Object2NodeMapperUnit.graphDatabaseService.shutdown();
-    }
-    finally {
-      tracer.wayout();
-    }
-  }
-
-  @Override
-  public AbstractTracer getCurrentTracer() {
-    return TracerFactory.getInstance().getCurrentPoolTracer();
-  }
 }
