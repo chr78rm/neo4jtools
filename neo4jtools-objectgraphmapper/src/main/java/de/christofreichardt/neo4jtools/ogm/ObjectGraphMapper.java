7e1ba4701f4aa8b2d6d07c8540fd41108de26d6a
chr78rm <chr78rm@onlinehome.de>
2015-11-16 15:31:41 +0100

    Unnecessary import removed.

--- neo4jtools-objectgraphmapper/src/main/java/de/christofreichardt/neo4jtools/ogm/ObjectGraphMapper.java
+++ neo4jtools-objectgraphmapper/src/main/java/de/christofreichardt/neo4jtools/ogm/ObjectGraphMapper.java
@@ -7,7 +7,6 @@ import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.RelationshipType;
-import org.neo4j.graphdb.ResourceIterator;
 
 /**
  *
